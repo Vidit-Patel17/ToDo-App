db71022cfa8e823a88941ac9c566af6ba49e45ba
Vidit-Patel17 <f20180733@goa.bits-pilani.ac.in>
2021-05-12 16:57:38 +0530

    Functional UI

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -10,19 +10,31 @@ const HomeScreen = () => {
     const storeTerm = () => {
         console.log({term});
     }
-    return <View>
-        <Text>HomeScreen now Visible</Text>
-        <TextBar
-            term={term}
-            onTermChange={setTerm}
-            onTermSubmit={storeTerm}   //call helper function which adds the text to Notes  
-        />
-        <Text>New Todo : {term} </Text>
+    return <View style= {{flexDirection: 'column'}}>
+        <View style = {styles.top} >
+            <TextBar
+                term={term}
+                onTermChange={setTerm}
+                onTermSubmit={storeTerm}   //call helper function which adds the text to Notes  
+            />
+            <Text>New Todo : {term} </Text>
+        </View>
+        <View style = {styles.bottom}>
+            <Text>//Different todo lists</Text>
+        </View>
     </View>
 }
 
 const styles = StyleSheet.create({
-
+    top:{
+        height: '22%'
+    },
+    bottom: {
+        height: '72%',
+        borderColor: 'grey',
+        borderWidth: 1,
+        margin: 10
+    }
 });
 
 export default HomeScreen

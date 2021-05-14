277a0d2846c63f88e0e87f21bf247b781e861d95
Vidit-Patel17 <f20180733@goa.bits-pilani.ac.in>
2021-05-14 15:02:47 +0530

    Added AsyncStorage retrival

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -3,19 +3,38 @@ import {View,Text,StyleSheet, FlatList,TouchableOpacity, ScrollView, Button} fro
 import TextBar from '../components/textBar';
 import AsyncStorage  from '@react-native-async-storage/async-storage';
 
+
+const get = async () => {
+    try {
+        const value = await AsyncStorage.getItem('notes') 
+        if(value !== null){
+            
+            const obj = JSON.parse(value)
+            console.log(obj.ToDo)
+            return obj.ToDo
+        }
+        else   
+            return []    
+    } 
+    catch (error) {
+        console.error(error);
+        return null;
+    }
+};
+
+
 const HomeScreen = () => {
     const [term,setTerm] = useState('');
-    const [ToDo,setTodo] = useState([]);
+    const [ToDo,setTodo] = useState(get());
     //term to take input
     //ToDo is an array of todo's which are term which is added on pressing enter
-
+    
     //addItem is called by onEndEditing from the input
     const addItem = () => {
         if(term){
             const newToDo = {name: term, done: false}
             setTodo([...ToDo,newToDo]);
             setTerm('');
-            console.log(ToDo)
         }
     } 
 
@@ -33,19 +52,28 @@ const HomeScreen = () => {
         setTodo([]);
     }
 
+    const _storeData = async () => {
+        try {
+          await AsyncStorage.setItem('notes',JSON.stringify({ToDo}));
+        } 
+        catch (error) {
+          console.log(error)
+        }
+      };
+
     //useEffect is used to convert the array and store it in the memory everytime it changes.
-    useEffect(() => {
-        AsyncStorage.setItem('notes',JSON.stringify({ToDo}))
+    useEffect( () => {
+        _storeData()
     }, [{ToDo}]); 
 
-    return <View style= {{flexDirection: 'column'}}>
+    return <View >
         <View style = {styles.top} >
             <TextBar
                 term={term}
                 onTermChange={setTerm}
                 onTermSubmit={() => addItem()}   //call helper function which adds the text to Notes  
             />
-            <Text>New Todo : {term} </Text>
+            <Text style={styles.txt}>New Todo : {term} </Text>
         </View>
         
         <View style = {styles.bottom}>
@@ -62,6 +90,7 @@ const HomeScreen = () => {
                             else{
                                 item.done = true;
                             }
+                            setTodo([...ToDo])
                         }}
                     >
                         <Text  style={[styles.todo, 
@@ -74,14 +103,18 @@ const HomeScreen = () => {
             </ScrollView>
         </View>
         <View style={styles.clearBTN}>
-            <Button
-                title="Clear Done"
+            <TouchableOpacity
+                style = {styles.btn}
                 onPress = {() => removeDone()}
-            />
-            <Button
-                title="Clear All"
+            >
+                <Text style = {styles.btnColor} >Clear Done</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+                style = {styles.btn}
                 onPress = {() => removeAll()}
-            />
+            >
+                <Text style = {styles.btnColor} >Clear All</Text>
+            </TouchableOpacity>
         </View>
 
     </View>
@@ -90,6 +123,11 @@ const HomeScreen = () => {
 
 
 const styles = StyleSheet.create({
+    txt:{
+        marginLeft:10,
+        fontSize: 20,
+        marginTop: 3
+    },
     top:{
         height: '22%'
     },
@@ -105,8 +143,16 @@ const styles = StyleSheet.create({
     },
     clearBTN: {
         flexDirection: 'row',
-        width: '100%',
-        alignContent: 'center'
+        width: '100%'
+    },
+    btn: {
+        flex :1,
+    },
+    btnColor: {
+        color: 'blue',
+        fontSize: 20,
+        marginVertical: 5,
+        alignSelf: 'center'
     }
 });
 
